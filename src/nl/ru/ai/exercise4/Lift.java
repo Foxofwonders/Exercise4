12cf562c84854f982d11bb403d881a008c191293
Unknown <denisevbaalen@gmail.com>
2016-12-09 15:14:18 +0100

    Made AnnaCode pretty

--- src/nl/ru/ai/exercise4/Lift.java
+++ src/nl/ru/ai/exercise4/Lift.java
@@ -6,30 +6,37 @@ public class Lift
 {
 	 public static int nrOfFails;
 	 public static int nrOfPrunes;
-	 public static int[] bestSolution = new int[6];
+	 
 
 	public static void main(String[] args)
 	  {
 		ArrayList<Integer> s = new ArrayList<Integer>( ); 
 	    int[] weights= { 30, 40, 41, 80, 90, 50, 55, 92, 66, 82, 62, 70};
+	    
 	    int people = weights.length;
 	    int targetWeight=500;
 	    int targetPeople=6;
-	    System.out.println("Total number of solutions: " + solutions(weights,people,0,targetWeight,targetPeople,s));
+	    int[] bestSolution = new int[targetPeople];
+	    System.out.println("Total number of solutions: " + solutions(weights,people,0,targetWeight,targetPeople,s, bestSolution));
 	    int bestSum=0;
 		 for(int i =0;i<bestSolution.length;i++)
 		 {
 			 bestSum=bestSum+bestSolution[i];
 		 }
-	    System.out.println("Best Solution: " + bestSolution[0] + " "+ bestSolution[1] + " "+ bestSolution[2] + " "+ bestSolution[3] + " "+ bestSolution[4] + " "+ bestSolution[5] + " = " + bestSum);
-	    System.out.println("Total number of Fails: " + nrOfFails);
+		 System.out.println("Best Solution: ");
+		 for (int i=0; i<bestSolution.length; i++)
+		 {
+			 System.out.print(bestSolution[i] + " ");
+		 }
+		 System.out.println(" = "+bestSum);
+	    
 	    System.out.println("Total number of Prunes: " + nrOfPrunes);
 	    nrOfFails=0;
 	    nrOfPrunes=0;
 	    System.out.println("");
 	  }
 
-	 private static int solutions(int[] weights,int people, int currentPerson, int targetWeight, int targetPeople, ArrayList<Integer> s)
+	 private static int solutions(int[] weights,int people, int currentPerson, int targetWeight, int targetPeople, ArrayList<Integer> s, int[] bestSolution)
 	  {
 	    assert weights!=null : "array should be initialized";
 	    assert currentPerson>=0 && currentPerson<=weights.length;
@@ -43,7 +50,7 @@ public class Lift
 	    }
 	    if(targetWeight>=0 && targetPeople==0)
 	    {
-	    	showSolution(s);
+	    	showSolution(s, bestSolution);
 		    return 1;
 	    }
 	    else if(pruneTest)
@@ -55,13 +62,13 @@ public class Lift
 	    
 	    
 	    s.add(weights[currentPerson]);
-	    int with = solutions(weights,people-1,currentPerson+1,targetWeight-weights[currentPerson],targetPeople-1, s);
+	    int with = solutions(weights,people-1,currentPerson+1,targetWeight-weights[currentPerson],targetPeople-1, s, bestSolution);
 	    s.remove(s.size()-1);
-	    int without = solutions(weights,people,currentPerson+1,targetWeight,targetPeople, s);
+	    int without = solutions(weights,people,currentPerson+1,targetWeight,targetPeople, s, bestSolution);
 	    
 	    return with+without;
 	  }
-	 private static void showSolution( ArrayList<Integer> s) 
+	 private static void showSolution( ArrayList<Integer> s, int[] bestSolution) 
 	  {
 		 int sum=0;
 		 int bestSum=0;
