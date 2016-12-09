09442e088f7405b5c231e302f324bca53683179a
Unknown <anna.gansen@outlook.com>
2016-12-09 14:18:29 +0100

    2 only Weights

--- src/nl/ru/ai/exercise4/Lift.java
+++ src/nl/ru/ai/exercise4/Lift.java
@@ -10,9 +10,11 @@ public class Lift
 	public static void main(String[] args)
 	  {
 		ArrayList<Integer> s = new ArrayList<Integer>( ); 
-	    int[] money0= { 30, 40, 41, 80, 90, 50, 55, 92, 66, 82, 62, 70};
-	    int target0=0;
-	    System.out.println("Total number of solutions: " + solutions(money0,0,target0,s));
+	    int[] weights= { 30, 40, 41, 80, 90, 50, 55, 92, 66, 82, 62, 70};
+	    int people = 13;
+	    int targetWeight=500;
+	    int targetPeople=6;
+	    System.out.println("Total number of solutions: " + solutions(weights,people,0,targetWeight,targetPeople,s));
 	    System.out.println("Total number of Fails: " + nrOfFails);
 	    System.out.println("Total number of Prunes: " + nrOfPrunes);
 	    nrOfFails=0;
@@ -20,9 +22,57 @@ public class Lift
 	    System.out.println("");
 	  }
 
-	private static String solutions(int[] money0, int i, int target0, ArrayList<Integer> s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	 private static int solutions(int[] weights,int people, int currentPerson, int targetWeight, int targetPeople, ArrayList<Integer> s)
+	  {
+	    assert weights!=null : "array should be initialized";
+	    assert currentPerson>=0 && currentPerson<=weights.length;
+	    int sumOfRest=0;
+	    boolean pruneTest = true;
+	    for(int i=currentPerson;i<weights.length;i++)
+	    {
+	    	sumOfRest = sumOfRest+weights[i];
+	    	if(weights[i] <= targetWeight)
+	    	{
+	    		pruneTest = false;
+	    	}
+	    }
+	    if(targetWeight==0)
+	    {
+	    	showSolution(s);
+		    return 1;
+	    }
+	    else if(pruneTest||sumOfRest<targetWeight)
+	    {
+	    	nrOfPrunes++;
+	    	return 0;
+	    }
+	    else if(targetWeight<0)
+	    {
+	    	nrOfFails++;
+	    	return 0;
+	    }
+	    
+	    else if(currentPerson>=weights.length)
+	    {
+	    	nrOfFails++;
+	    	return 0;
+	    }
+	    
+	    s.add(weights[currentPerson]);
+	    int with = solutions(weights,people,currentPerson+1,targetWeight-weights[currentPerson],targetPeople, s);
+	    s.remove(s.size()-1);
+	    int without = solutions(weights,people,currentPerson+1,targetWeight,targetPeople, s);
+	    
+	    return with+without;
+	  }
+	 private static void showSolution( ArrayList<Integer> s) 
+	  {
+		 int sum=0;
+		 for(int i =0;i<s.size();i++)
+		 {
+			 sum=sum+s.get(i);
+		 }
+		  System.out.println("One possible solution: " + s + " = " + sum);
+	  }
 
 }
